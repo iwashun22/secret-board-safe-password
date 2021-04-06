1ab875a4a81778348d7fcf9b8322dddcbaf78896
taigaozawa <taigaozawa@gmail.com>
2021-04-06 19:35:00 +0900

    Docker対応

--- test.js
+++ test.js
@@ -4,18 +4,19 @@ const assert = require('assert');
 
 // pug のテンプレートにおける XSS 脆弱性のテスト
 const html = pug.renderFile('./views/posts.pug', {
-  posts: [{
-    id: 1,
-    content: '<script>alert(\'test\');</script>',
-    postedBy: 'guest1',
-    trackingCookie: 1,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }],
+  posts: [
+    {
+      id: 1,
+      content: "<script>alert('test');</script>",
+      postedBy: 'guest1',
+      trackingCookie: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ],
   user: 'guest1'
 });
 
 // スクリプトタグがエスケープされて含まれていることをチェック
-assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+assert(html.includes("&lt;script&gt;alert('test');&lt;/script&gt;"));
 console.log('テストが正常に完了しました');
-
